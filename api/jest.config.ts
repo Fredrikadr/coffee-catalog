13e00e068193663d8235cc625fa369c5bff08436
fredrikadr <fredrikadr@gmail.com>
2023-06-28 13:30:49 +0200

    configured jest

---
+++ api/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    displayName: "Unit Tests",
+    testMatch: ["<rootDir>/tests/unit/**/*.test.ts"],
+  },
+  {
+    displayName: "Integration Tests",
+    testMatch: ["<rootDir>/tests/integration/**/*.test.ts"],
+  },
+];
