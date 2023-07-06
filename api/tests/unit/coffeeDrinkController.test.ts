b6aadf717b6f426d92ee4be92d49921067d9b989
fredrikadr <fredrikadr@gmail.com>
2023-07-06 19:21:55 +0200

    unit tests for controllers

---
+++ api/tests/unit/coffeeDrinkController.test.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import CoffeeDrinkController from "../../src/controllers/CoffeeDrinkController";
+import coffeeData from "../../../data/CoffeeData.json";
+import { formatCoffeeData } from "../../src/utils/coffeeDataMapper";
+
+jest.mock("../../src/utils/coffeeDataMapper", () => ({
+  formatCoffeeData: jest.fn((rawData: any[]) => []),
+}));
+
+
+
+describe("CoffeeDrinkController", () => {
+  let coffeeDrinkController: CoffeeDrinkController;
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
+    coffeeDrinkController = new CoffeeDrinkController();
+    req = {} as Request;
+    res = {} as Response;
+    res.status = jest.fn().mockReturnThis();
+    res.json = jest.fn();
+  });
+
+  it("should list all coffee drinks", () => {
+    coffeeDrinkController.listAllCoffeeDrinks(req, res);
+
+    expect(formatCoffeeData).toHaveBeenCalledWith(coffeeData);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(expect.any(Array));
+  });
+
+  it("should get a coffee drink by ID", () => {
+    req.params = { id: "123" };
+
+    const coffeeDrinkMock = { id: 123, name: "Coffee Drink" };
+    jest.spyOn(Array.prototype, "find").mockReturnValueOnce(coffeeDrinkMock);
+
+    coffeeDrinkController.getCoffeeDrinkById(req, res);
+
+    expect(formatCoffeeData).toHaveBeenCalledWith(coffeeData);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(coffeeDrinkMock);
+  });
+
+  it("should return a 404 error if coffee drink is not found", () => {
+    req.params = { id: "123" };
+
+    jest.spyOn(Array.prototype, "find").mockReturnValueOnce(undefined);
+
+    coffeeDrinkController.getCoffeeDrinkById(req, res);
+
+    expect(formatCoffeeData).toHaveBeenCalledWith(coffeeData);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({
+      code: 404,
+      message: "Coffee drink not found",
+    });
+  });
+});
