9067482540d0bf5afb904cf9f9d38cca39e1b603
fredrikadr <fredrikadr@gmail.com>
2023-07-10 09:26:13 +0200

    add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import coffeeDrinkRoutes from "./routes/coffeeDrinkRoutes"
+import cors from "cors";
+
 
 const app = express();
 
+app.use(cors());
 app.use(coffeeDrinkRoutes);
 
 export default app;
