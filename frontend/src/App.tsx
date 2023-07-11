656dd4881fa58119b99dbb6a23e6eb51c98c6499
fredrikadr <fredrikadr@gmail.com>
2023-07-11 13:39:08 +0200

    re-config scss and add more styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import CoffeeCatalog from './components/CoffeeCatalog.tsx'
-import './styles.scss'
+
 
 function App() {
 
   return (
     <>
       <div id="glow-container">
-      <div id="glow-effect"></div>
-
+        <div id="glow-effect"></div>
       </div>
-      <CoffeeCatalog/>
+      
+      <CoffeeCatalog />
     </>
   )
 }
